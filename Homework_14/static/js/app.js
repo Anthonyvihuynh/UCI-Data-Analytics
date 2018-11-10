432140270e4703f08ac3b346d16d221b0692f2a2
Anthony Huynh <ANTHONYVIHUYNH@GMAIL.COM>
2018-11-10 11:58:46 -0800

    Updating HW 14

--- Homework_14/static/js/app.js
+++ Homework_14/static/js/app.js
@@ -1,40 +1,60 @@
 // from data.js
 var tableData = data;
-var dateInput = d3.select('#datetime');
-var searchBtn = d3.select('#filter-btn');
 
 // // YOUR CODE HERE!
+var tbody = d3.select("tbody");
 var table = d3.select("#ufo-table");
 
 // $searchBtn.addEventListener('click', dateSearch);
 
-function renderTable() {
-	for (var i = 0; i < tableData.length; i++) {
-		var date = tableData[0];
-		var fields = Object.keys(date);
-		// var row = table.insertRow(i)
-	    for (var j = 0; j < fields.length; j++) {
-	    	var datum = fields[j];
-	    	table.append(datum);
-	};
+// function renderTable() {
+// 	for (var i = 0; i < tableData.length; i++) {
+// 		var date = tableData[0];
+// 		var fields = Object.keys(date);
+// 		// var row = table.insertRow(i)
+// 	    for (var j = 0; j < fields.length; j++) {
+// 	    	var datum = fields[j];
+// 	    	table.append(datum);
+// 	};
+// };
+// };
+
+
+// function buildTable(data) {
+//   table.html("");
+//   data.forEach((dataRow) => {
+//     var row = table.append("tr");
+//     Object.values(datum).forEach((datum) => {
+//       var cell = row.append("td");
+//         cell.text(val);
+//       }
+//     );
+//   });
+// }
+
+function buildTable(data) {
+  tbody.html("");
+  data.forEach((datum) => {
+    var row = tbody.append("tr");
+    Object.values(datum).forEach((dataCell) => {
+      var cell = row.append("td");
+        cell.text(dataCell);
+      }
+    );
+  });
+}
+
+
+function handleClick() {
+  d3.event.preventDefault();
+  var date = d3.select("#datetime").property("value");
+    searchDate = tableData;
+	if (date) {
+    	searchDate = searchDate.filter(row => row.datetime === date);
+	}
+  buildTable(searchDate);
 };
-};
 
+d3.selectAll("#filter-btn").on("click", handleClick);
 
-
-// data.forEach((tableData) => {
-// 	var row = table.append("tr");
-// 	Object.entries(tableData).forEach(([datetime, city, state, country, shape, comment]) => {
-// 		var cell = table.append("td");
-// 		cell.text(city);
-// 		var date = tableData[0]
-// 	});
-// });
-
-
-
-searchBtn.on("click", function() {
-	d3.event.preventDefault();
-	var filteredDate = tableData.filter(tableData => tableData.datetime === datetime)
-	renderTable(filteredDate);
-});
+buildTable(tableData);
